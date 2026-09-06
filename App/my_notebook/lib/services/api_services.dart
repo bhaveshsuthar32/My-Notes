3ffa333d736bc5fc795f54bc0eeeb0e385ed2b53
Bhavesh Suthar <bagrabhavesh426@gmail.com>
2026-09-06 14:47:51 +0530

    feat: add get topic and register APIs

--- App/my_notebook/lib/services/api_services.dart
+++ App/my_notebook/lib/services/api_services.dart
@@ -13,46 +13,86 @@ class ApiServices {
 
         return decoded["data"];
       } else {
-       throw Exception(
-          "Failed to load Notes data: ${res.statusCode}",
-        );
+        throw Exception("Failed to load Notes data: ${res.statusCode}");
       }
     } catch (e) {
       throw Exception("Error : $e");
     }
   }
 
-
   // login
-Future<Map<String, dynamic>> loginApi(
-  String email,
-  String password,
-) async {
-  try {
-    final res = await http.post(
-      Uri.parse('$baseURL/login'),
+  Future<Map<String, dynamic>> loginApi(String email, String password) async {
+    try {
+      final res = await http.post(
+        Uri.parse('$baseURL/login'),
 
-      headers: {
-        'Content-Type': 'application/json',
-      },
+        headers: {'Content-Type': 'application/json'},
 
-      body: jsonEncode({
-        'email': email,
-        'password': password,
-      }),
-    );
-
-    if (res.statusCode == 200) {
-      final decoded = jsonDecode(res.body);
-
-      return decoded;
-    } else {
-      throw Exception(
-        'Login failed: ${res.statusCode}',
+        body: jsonEncode({'email': email, 'password': password}),
       );
+
+      if (res.statusCode == 200) {
+        final decoded = jsonDecode(res.body);
+
+        return decoded;
+      } else {
+        throw Exception('Login failed: ${res.statusCode}');
+      }
+    } catch (e) {
+      throw Exception('Login error: $e');
     }
-  } catch (e) {
-    throw Exception('Login error: $e');
   }
-}
+
+  // Register
+  Future<Map<String, dynamic>> RegisterAPI(
+    String firstname,
+    String lastname,
+    String email,
+    String password,
+  ) async {
+    try {
+      final res = await http.post(
+        Uri.parse('$baseURL/register'),
+        headers: {'Content-Type': 'application/json'},
+        body: jsonEncode({
+          'firstname': firstname,
+          'lastname': lastname,
+          'email': email,
+          'password': password,
+        }),
+      );
+
+      print("Register Status: ${res.statusCode}");
+      print("Register Response: ${res.body}");
+
+      if (res.statusCode == 200 || res.statusCode == 201) {
+        final decoded = jsonDecode(res.body);
+        return decoded;
+      } else {
+        throw Exception('Register failed: ${res.statusCode}\n${res.body}');
+      }
+    } catch (e) {
+      throw Exception('Register error: $e');
+    }
+  }
+
+  // getTopic
+
+  Future<List<dynamic>> getTopicsAPI() async {
+    try {
+      final res = await http.get(
+        Uri.parse('$baseURL/getTopic')
+      );
+
+      if(res.statusCode == 200){
+        final decoded = jsonDecode(res.body);
+        return decoded["data"];
+      }else {
+        throw Exception("Failed to load Topics data: ${res.statusCode}");
+      }
+    } catch (e) {
+      throw Exception("Error: $e");
+    }
+  }
+
 }
